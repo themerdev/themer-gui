88ceaecc807db0c7afeedf35fc74d8864a2c3bcd
Matt Swensen <swensen.matthew@gmail.com>
2017-05-03 19:55:16 -0600

    Adding blur effect to main UI layer on dialog open.

--- app/components/App.js
+++ app/components/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
 import ColorSet from './ColorSet';
 import Dialogs from './Dialogs';
 import Menu from './Menu';
 import css from './App.css';
 
-export default () => (
+const App = ({ dialogOpen }) => (
   <section className={ css.gui }>
-    <div className={ css.colorSetContainer }>
-      <ColorSet />
-      <ColorSet light={ true } />
+    <div className={ classnames(css.mainUiLayer, { [css.obscured]: dialogOpen }) }>
+      <div className={ css.colorSetContainer }>
+        <ColorSet />
+        <ColorSet light={ true } />
+      </div>
+      <Menu />
     </div>
-    <Menu />
     <Dialogs />
   </section>
 );
+
+const mapStateToProps = state => ({
+  dialogOpen: Object.values(state.dialogsVisibility).some(v => v),
+});
+
+export default connect(
+  mapStateToProps,
+)(App);
