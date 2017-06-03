b84cc0988325e01dc67822ffac4090f78b4943b1
Matt Swensen <swensen.matthew@gmail.com>
2017-06-03 13:01:58 -0600

    Resolving a couple TODOs.

--- main/save.js
+++ main/save.js
@@ -1,6 +1,7 @@
 const { app, dialog, ipcMain } = require('electron');
 const path = require('path');
 const fs = require('fs');
+const _ = require('lodash');
 const {
   SAVE_REQUEST,
   SAVE_AS_REQUEST,
@@ -12,40 +13,14 @@ const defaultBasename = 'Untitled';
 const ext = 'themer'; // TODO: get from package.json
 const defaultFilename = `${defaultBasename}.${ext}`;
 
-// const isModified = (filePath, contents) => {};
-// 
-// const save = (filePath, contents) => {
-//   fs.writeFile(filePath, contents);
-//   // TODO: or, send it back first, wait for state to get set, and then write the file with the new handle as part of the state.
-//   // START HERE: ^ this is probably the right way to do it.
-// };
-// 
-// const saveAs = (browserWindow, contents) => {
-//   dialog.showSaveDialog(
-//     browserWindow,
-//     {
-//       title: 'Save As...', // TODO: try without
-//       defaultPath: path.join(app.getPath('home'), defaultFilename),
-//     },
-//     (userSavePath) => {
-//       save(userSavePath, contents);
-//       // TODO: send back userSavePath
-//     }
-//   );
-// };
-// 
-// const open = (filePath) => {
-//   // If there are changes, ask to save current buffer, with option not to.
-//   // If there are no changes (empty new buffer or unmodified buffer with file handle)
-//   // open and set file handle.
-// };
-
-////////////////
-// NEW API BELOW
-//////////////////
-
 const isModified = (filePath, contents) => new Promise((resolve, reject) => {
-  reject('TODO not yet implemented');
+  fs.readFile(filePath, 'utf8', (err, writtenData) => {
+    if (err) { reject(err); }
+    else {
+      try { resolve(_.isEqual(JSON.parse(writtenData), JSON.parse(contents))); }
+      catch(e) { reject(e); }
+    }
+  });
 });
 
 const promptForFilePath = browserWindow => new Promise((resolve, reject) => {
@@ -74,7 +49,7 @@ exports.bootstrap = (browserWindow) => {
 
   ipcMain.on(SAVE_REQUEST, (event, filePath, data) => {
     const contents = JSON.stringify(data);
-    if (filePath) { // TODO: move all these into methods
+    if (filePath) {
       writeFile(filePath, contents)
         .then(pathWritten => event.sender.send(SAVE_COMPLETE, pathWritten))
         .catch(e => e && event.sender.send(SAVE_ERROR, e));
@@ -95,20 +70,4 @@ exports.bootstrap = (browserWindow) => {
       .catch(e => e && event.sender.send(SAVE_ERROR, e));
   });
 
-  // ipcMain.on(SAVE_REQUEST, (event, state) => {
-  //   const filePath = state.filePath;
-  //   const applicationState = Object.assign({}, state);
-  //   delete applicationState.filePath;
-  //   // TODO: replace above with below line once webpack/babel are set up for main process, too
-  //   // const { filePath, ...applicationState } = state;
-  //   if (!filePath) {
-  //     saveAs(browserWindow, JSON.stringify(applicationState));
-  //   }
-  //   else {
-  //     save(filePath, JSON.stringify(applicationState));
-  //   }
-  // });
-  // ipcMain.on(SAVE_AS_REQUEST, (event, state) => {
-  //   saveAs(browserWindow, JSON.stringify(state));
-  // });
 };
