35c40dc5a6d4a6dcc30ea94bb573697cca27dba0
Matt Swensen <swensen.matthew@gmail.com>
2017-08-31 22:47:20 -0600

    Try docker to build Linux/Windows

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  docker run --rm -ti -v ${PWD}:/project -v ${PWD##*/}-node-modules:/project/node_modules \
+    -v ~/.cache/electron:/root/.cache/electron \
+    ~/.cache/electron-builder:/root/.cache/electron-builder \
+    electronuserland/electron-builder:wine /bin/bash -c "yarn && yarn dist"
+fi
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  yarn && yarn dist
+fi
