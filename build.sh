92b80619f37453661263ecad6c2656eaa80e1ede
Matt Swensen <swensen.matthew@gmail.com>
2017-08-31 22:58:43 -0600

    Fix typo in docker command

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -ev
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   docker run --rm -ti -v ${PWD}:/project -v ${PWD##*/}-node-modules:/project/node_modules \
     -v ~/.cache/electron:/root/.cache/electron \
-    ~/.cache/electron-builder:/root/.cache/electron-builder \
+    -v ~/.cache/electron-builder:/root/.cache/electron-builder \
     electronuserland/electron-builder:wine /bin/bash -c "yarn && yarn dist"
 fi
 
