6bccb6e826777ef05b549b9e826ab081075a1794
Matt Swensen <swensen.matthew@gmail.com>
2017-09-01 23:36:30 -0600

    Try building windows bundle on OSX instead

--- build.sh
+++ build.sh
@@ -2,12 +2,10 @@
 set -ev
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  sudo add-apt-repository ppa:ubuntu-wine/ppa -y
-  sudo apt-get update
-  sudo apt-get install --no-install-recommends -y icnsutils graphicsmagick xz-utils wine1.8
-  yarn && yarn dist -- --linux --win
+  sudo apt-get install --no-install-recommends -y icnsutils graphicsmagick xz-utils
+  yarn && yarn dist
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  yarn && yarn dist
+  yarn && yarn dist -- --mac --win
 fi
