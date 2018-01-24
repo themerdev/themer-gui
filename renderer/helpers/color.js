c46e6ede669a5f5d618dcf8e9a24c13e4c852d00
Matt Swensen <swensen.matthew@gmail.com>
2018-01-24 06:36:12 -0700

    Refactor distribute shades calculation to ues color-steps package.

--- renderer/helpers/color.js
+++ renderer/helpers/color.js
@@ -1,5 +1,6 @@
 import Color from 'color';
 import { memoize, range } from 'lodash';
+import colorSteps from 'color-steps';
 
 export const getOrDefault = memoize((maybeColor, fallback) => {
   try {
@@ -23,18 +24,6 @@ export const getBestForeground = memoize((option1, option2, background) => {
   }
 }, (option1, option2, background) => `${option1}:${option2}:${background}`);
 
-export const distribute = (color1, color2, count = 8) => {
-  const [r1, g1, b1] = Color(color1).rgb().array();
-  const [r2, g2, b2] = Color(color2).rgb().array();
-  const rInterval = (r2 - r1) / (count - 1);
-  const gInterval = (g2 - g1) / (count - 1);
-  const bInterval = (b2 - b1) / (count - 1);
-  return range(1, count-1)
-    .map(i => [
-      Math.round(r1 + rInterval * i),
-      Math.round(g1 + gInterval * i),
-      Math.round(b1 + bInterval * i),
-    ])
-    .map(Color.rgb)
-    .map(c => c.hex());
+export const distribute = (color1, color2) => {
+  return colorSteps(color1, color2).map(c => Color(c).hex());
 };
