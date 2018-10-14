f9d6e8b85b192630f7a19913ca555cc477d38a35
Reed Jones <reedjones@reedjones.com>
2018-10-14 13:27:56 -0600

    fix incorrect path to usage instructions

--- renderer/helpers/options.js
+++ renderer/helpers/options.js
@@ -1,67 +1,67 @@
 export const options = {
   hyper: {
     name: 'Hyper',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-hyper#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-hyper#output',
   },
   iterm: {
     name: 'iTerm',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-iterm#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-iterm#output',
   },
   terminal: {
     name: `Terminal.app${process.platform === 'darwin' ? '' : ' (macOS only)'}`,
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-terminal#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-terminal#output',
   },
   atomSyntax: {
     name: 'Atom syntax',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-atom-syntax#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-atom-syntax#output',
   },
   sublimeText: {
     name: 'Sublime Text',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-sublime-text#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-sublime-text#output',
   },
   vim: {
     name: 'Vim',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-vim#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-vim#output',
   },
   vimLightline: {
     name: 'Vim lightline',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-vim-lightline#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-vim-lightline#output',
   },
   vsCode: {
     name: 'VS Code',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-vscode#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-vscode#output',
   },
   slack: {
     name: 'Slack sidebar',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-slack#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-slack#output',
   },
   conEmu: {
     name: 'ConEmu',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-conemu#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-conemu#output',
   },
   cmd: {
     name: 'CMD.exe',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-cmd#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-cmd#output',
   },
   atomUi: {
     name: 'Atom UI',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-atom-ui#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-atom-ui#output',
   },
   xcode: {
     name: 'Xcode',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-xcode#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-xcode#output',
   },
   bbEdit: {
     name: 'BBEdit',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-bbedit#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-bbedit#output',
   },
   alfred: {
     name: 'Alfred.app',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-alfred#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-alfred#output',
   },
   chrome: {
     name: 'Chrome',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-chrome#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-chrome#output',
   },
   jetBrains: {
     name: 'JetBrains',
@@ -69,7 +69,7 @@ export const options = {
   },
   sketchPalettes: {
     name: 'Sketch palettes',
-    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/packages/themer-sketch-palettes#output',
+    instructionsUrl: 'https://github.com/mjswensen/themer/tree/master/cli/packages/themer-sketch-palettes#output',
   },
   tmux: {
     name: 'tmux',
